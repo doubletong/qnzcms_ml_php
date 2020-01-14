773e770b8ba8f79d7ac78e2f681506b604062802
tongjugang <ytjang@msn.com>
2020-01-14 19:53:51 +0800

    mobile

---
+++ views/statement/legal.php
@@ -0,0 +1,50 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/common.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/loadCommonData.php");
+require_once($_SERVER['DOCUMENT_ROOT'] ."/data/page.php");
+
+$pageClass = new TZGCMS\Page();
+
+//twig 模板设置
+$loader = new \Twig\Loader\FilesystemLoader(array('../../assets/templates'));
+
+if($site_info['enableCaching']=="1"){
+
+    $twig = new \Twig\Environment($loader, [
+        'cache' => '../../assets/caches',
+    ]);
+    // In your class, function, you can call the Cache
+    // $InstanceCache = CacheManager::getInstance('files');
+
+    $key = "statement_legal";
+    $CachedString = $InstanceCache->getItem($key);   
+
+    if (!$CachedString->isHit()) {
+
+        $page = $pageClass->fetch_data("legal");   
+        $home_data = ['page' => $page];
+
+        $CachedString->set($home_data)->expiresAfter(5000);//in seconds, also accepts Datetime
+        $InstanceCache->save($CachedString); // Save the cache item just like you do with doctrine and entities
+ 
+        //echo $CachedString->get();
+    }
+
+    $result = $CachedString->get();
+
+}else{
+    $twig = new \Twig\Environment($loader);  
+    $page = $pageClass->fetch_data("legal");   
+    $result = ['page' => $page];
+}
+
+
+$twig->addGlobal('site', $site_info);
+$twig->addGlobal('menus', $menutree);
+$twig->addGlobal('navbot', $menus_bot);
+$twig->addGlobal('uri', $uri);
+
+
+echo $twig->render('statement/legal.html', $result);
+
+?>
